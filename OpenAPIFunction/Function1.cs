e43e9b3ae0853c2b6f6ff1fc605eeb84765bae68
Vittorio Passuello-Dussault <vittorio.passduss@lefewaresolutions.com>
2021-05-12 11:42:10 -0400

    boilerplate

---
+++ OpenAPIFunction/Function1.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using AzureFunctions.Extensions.Swashbuckle;
+
+namespace OpenAPIFunction
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [SwaggerIgnore]
+        [FunctionName("Swagger")]
+        public static Task<HttpResponseMessage> Run2(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "Swagger/json")] HttpRequestMessage req,
+            [SwashBuckleClient] ISwashBuckleClient swashBuckleClient)
+        {
+            return Task.FromResult(swashBuckleClient.CreateSwaggerJsonDocumentResponse(req));
+        }
+
+        [SwaggerIgnore]
+        [FunctionName("SwaggerUi")]
+        public static Task<HttpResponseMessage> Run3(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "Swagger/ui")] HttpRequestMessage req,
+            [SwashBuckleClient] ISwashBuckleClient swashBuckleClient)
+        {
+            return Task.FromResult(swashBuckleClient.CreateSwaggerUIResponse(req, "swagger/json"));
+        }
+    }
+}
---
+++ OpenAPIFunction/SwashBuckleStartup.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using AzureFunctions.Extensions.Swashbuckle;
+using OpenAPIFunction;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: WebJobsStartup(typeof(SwashBuckleStartup))]
+namespace OpenAPIFunction
+{
+    public class SwashBuckleStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
+        }
+    }
+}
